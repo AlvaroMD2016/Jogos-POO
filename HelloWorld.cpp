48a0d3ecaf4831dde502b126956a1ba41c7be46a
AlvaroMD2016 <alvarodantas@hotmail.com>
2016-06-08 19:41:14 -0300

    POOAulas
    HelloWorld-comentado

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+//Meu primeiro código em C++
+
+//Entrando na biblioteca
+#include<iostream>
+using namespace std;
+
+int main(int agrc, char* agrs[])
+{
+	//Imprimindo na tela
+	cout << "HelloWorld" << endl;
+
+	return 0;
+}
